00c41b925e1b3af7065e34550592a4bf6b52bf6b
kevit-dev-bhayani <dev.bhayani@kevit.io>
2023-10-19 18:55:39 +0530

    implement ping  router with class

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,27 @@
 require('dotenv').config();
-import express, {Application} from 'express';
+import express from 'express';
+import {Application, Router} from 'express';
+import {server} from './config';
+import {logger} from './utils/logger';
+export class App {
+  public app: Application;
+  public routers: Router[];
 
-export const app: Application = express();
+  constructor(routers: Router[]) {
+    this.routers = routers;
+    // this.listen();
+    this.app = express();
+    this.initializeRoute();
+  }
+  listen() {
+    this.app.listen(server.port, () => {
+      logger.info(`app is running on port ${server.port}`);
+    });
+  }
+  initializeRoute() {
+    this.routers.forEach((router) => {
+      this.app.use('/', router);
+    });
+  }
+}
+// export const app: Application = express();
