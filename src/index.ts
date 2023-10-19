ea8935060f42ac955a1cdec1a4fba60ba4d42ff8
kevit-dev-bhayani <dev.bhayani@kevit.io>
2023-10-19 12:57:51 +0530

    implement env config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import {app} from './app';
+import {server} from './config';
 
-app.listen(3000,()=>{
-    console.log("app is running");
+
+app.listen(server.port,()=>{
+    console.log("app is running " + server.port);
 })
