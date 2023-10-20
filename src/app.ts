8514ac0c1ca0e3d52492a42f6462067f99abfee0
kevit-dev-bhayani <dev.bhayani@kevit.io>
2023-10-20 11:29:48 +0530

    db setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 require('dotenv').config();
 import express from 'express';
+import mongoose from 'mongoose';
 import {Application, Router} from 'express';
-import {server} from './config';
+import {server, mongoConfig} from './config';
 import {logger} from './utils/logger';
 export class App {
   public app: Application;
@@ -12,6 +13,7 @@ export class App {
     // this.listen();
     this.app = express();
     this.initializeRoute();
+    this.mongooseSetup();
   }
   listen() {
     this.app.listen(server.port, () => {
@@ -22,6 +24,22 @@ export class App {
     this.routers.forEach((router) => {
       this.app.use('/', router);
     });
+  }
+
+  mongooseSetup() {
+    mongoose.connection.on('connected', () => {
+      logger.info('DB connected...!');
+    });
+
+    mongoose.connection.on('error', (error) => {
+      logger.error(`DB error - ${error}`);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+      logger.warn(`DB disconnected...!`);
+    });
+
+    mongoose.connect(`${mongoConfig.mongoUrl}${mongoConfig.dbName}`);
   }
 }
 // export const app: Application = express();
